93f7d54fb6467f4466c1f13be3db658d317996fd
unknown <khemilioussama775@gmail.com>
2020-12-26 21:31:09 +0100

    jointues 3

--- Jointures.sql
+++ Jointures.sql
@@ -11,3 +11,10 @@ RIGHT JOIN Race
     ON Animal.race_id = Race.id
 WHERE Race.espece_id = 2
 ORDER BY Race.nom, Animal.nom;
+
+
+select A.nom AS NOM_ANIMAL , R.NOM AS NOM_RACE, P.NOM AS PAPA , M.NOM AS MAMAN 
+FROM ANIMAL A 
+INNER JOIN RACE R ON A.race_id= R.id
+INNER JOIN ANIMAL P ON A.PERE_id=P.id
+inner join ANIMAL M on A.MERE_ID=M.id
